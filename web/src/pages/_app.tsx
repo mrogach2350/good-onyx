371adfd1631f977d63ef964e07c9e57e8ab5fbfa
Michael Rogachevsky <m.rogach@gmail.com>
2025-01-21 22:17:29 -0800

    adds auth to FE

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
 import {
@@ -6,10 +5,19 @@ import {
   QueryClientProvider,
   HydrationBoundary,
 } from "@tanstack/react-query";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import "bulma/css/bulma.min.css";
+import "@/styles/globals.css";
+import NavBar from '../components/NavBar'
 
 config.autoAddCss = false;
 
@@ -18,11 +26,14 @@ const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <HydrationBoundary state={pageProps.dehydratedState}>
-        <Component {...pageProps} />
-      </HydrationBoundary>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <ClerkProvider>
+      <QueryClientProvider client={queryClient}>
+        <HydrationBoundary state={pageProps.dehydratedState}>
+          <NavBar />
+          <Component {...pageProps} />
+        </HydrationBoundary>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </ClerkProvider>
   );
 }
