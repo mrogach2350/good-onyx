427bd0fb1ae2fe3c882f3119553b65dd6f4e3dd9
Michael Rogachevsky <m.rogach@gmail.com>
2025-02-09 19:27:19 -0800

    updates server listener

--- web/src/components/ServerListener.tsx
+++ web/src/components/ServerListener.tsx
@@ -1,14 +1,21 @@
 import { useEffect } from "react";
+import { useQueryClient } from "@tanstack/react-query";
 
-const NEXT_PUBLIC_BASE_SERVER_URL = process.env.NEXT_PUBLIC_BASE_SERVER_URL || "http://server:4000";
+const NEXT_PUBLIC_BASE_SERVER_URL =
+  process.env.NEXT_PUBLIC_BASE_SERVER_URL || "http://server:4000";
 
 export default function ServerListener() {
+  const queryClient = useQueryClient();
+
   useEffect(() => {
-    const eventSource = new EventSource(`${NEXT_PUBLIC_BASE_SERVER_URL}/events`);
+    const eventSource = new EventSource(
+      `${NEXT_PUBLIC_BASE_SERVER_URL}/events`
+    );
 
     eventSource.onmessage = (event) => {
       const data = JSON.parse(event.data);
       console.log("Data:", { ...data });
+      queryClient.invalidateQueries({ queryKey: ["vehicles"] });
     };
 
     return () => {
